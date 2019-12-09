122ae990dc7a4c54f54fe4de1c5bda77c0ebb435
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 16:49:06 -0500

    Add ASCII banner docblock

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+ * ######################################################
+ * ##    Chapter 2, Programming Exercise #5            ##
+ * ##    Developer: Matthew Obert                      ##
+ * ##    Date Submitted: December 9 2019               ##
+ * ##    Purpose: Width & numeric format specifiers    ##
+ * ######################################################
+ */
+using System;
 using static System.Console;
 
 namespace shirt_markdown
