451c375f91a51c87ff7522f5fc86c6cfa97d1a7e
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 16:47:49 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace shirt_markdown
 {
@@ -6,7 +7,7 @@ namespace shirt_markdown
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
